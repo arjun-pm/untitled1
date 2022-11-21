ee0d8cd2f00e6a8a4117e1ac32c591cb7f7d83f6
arjun-pm <arjunpm66@gmail.com>
2022-11-21 15:14:42 +0530

    commit

---
+++ bin/variables.dart
@@ -0,0 +1,14 @@
+void main(){
+  string name="Anu";
+  int   age   =20;
+  int phone   =23687347682;
+  double mark =8.3;
+  string email="anu@gmail.com";
+
+  print("----My Resume----");
+  print("My name is $name");
+  print("i am $age yrs old");
+  print("My phone number is $phone");
+  print("i have $mark cgpa in my graduation");
+  print("My email id is $email");
+}
